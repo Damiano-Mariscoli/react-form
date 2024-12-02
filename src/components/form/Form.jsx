11e3e25ca6ca787d76a2fd9495b524aca00ecef8
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-12-02 16:25:46 +0100

    ADDED: input dinamico, mostra in console quello che scrivi

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 function Form() {
+  const [title, setTitle] = useState("");
   return (
     <>
       <h2 className="text-center mb-3">Aggiungi un titolo:</h2>
       <form className="d-flex mb-3 justify-content-center gap-3" action="title">
-        <input type="text" />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
         <button className="btn btn-dark" type="submit">
           Add
         </button>
+        {console.log({ title })}
+        <p>il titolo è:{title}</p>
       </form>
     </>
   );
